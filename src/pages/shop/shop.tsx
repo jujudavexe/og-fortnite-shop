159b96b63d80d215acc58c3784fbe5d966077dbe
jujudave <jujudave1105@outlook.fr>
2023-12-03 22:21:37 +0100

    commit

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,19 +1,28 @@
 import './shop.css'
 
-const items:string[] = ['Soldat au crâne', 'Faucheuse', 'baguette étoilée', 'Cristal', 'éclaireuse galaxy', 'Aile articulée', 'Terreur sombre', 'Voleur de coeur']
+//const items:string[] = ['Soldat au crâne', 'Faucheuse', 'baguette étoilée', 'Cristal', 'éclaireuse galaxy', 'Aile articulée', 'Terreur sombre', 'Voleur de coeur']
+import './shop.css';
+
+const items: string[] = ['Soldat au crâne', 'Faucheuse', 'baguette étoilée', 'Cristal'];
+const colors: string[] = ['red', 'blue', 'green', 'purple'];
 
 function Shop() {
     return <div className={'background-container'}>
         <ShopContent items={items} hidden={false}></ShopContent>
     </div>
 }
-function ShopContent({items, hidden}: {items: string[], hidden: boolean}) {
-    if(hidden){
-        return null
+
+function ShopContent({ items, hidden }: { items: string[], hidden: boolean }) {
+    if (hidden) {
+        return null;
     }
-    return <ul className={'items'}>
-        {items.map((item: string) => (<li className={'item-box'}>{item}</li>))}
-    </ul>
+    return (
+        <ul className={'items'}>
+            {items.map((item: string, index: number) => (
+                <li className={'item-box'} style={{ borderColor: colors[index] }}>{item}</li>
+            ))}
+        </ul>
+    );
 }
 
-export default Shop
+export default Shop;

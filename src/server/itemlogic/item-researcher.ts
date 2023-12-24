9e99a8288e5d5e3333fa1803a1ffef3c1c8711e3
jujudave <jujudave1105@outlook.fr>
2023-12-24 20:02:41 +0100

    commit

--- src/server/itemlogic/item-researcher.ts
+++ src/server/itemlogic/item-researcher.ts
@@ -1,32 +1,28 @@
 // @ts-ignore
 import FortniteAPI from "fortnite-api-io";
-//import { Item } from "./item.ts"
+import { Item } from "./item.ts"
 
-async function getAndLogItems() {
-    // Instantiate with API Credentials
-    const client = new FortniteAPI("274cfc6f-21cd8529-7bcbbb4f-0c5751b9", {
-        defaultLanguage: 'fr',
-        ignoreWarnings: false
-    });
+const client = new FortniteAPI("274cfc6f-21cd8529-7bcbbb4f-0c5751b9", {
+    defaultLanguage: 'fr',
+    ignoreWarnings: false
+});
 
+async function getFeaturedItem(quantity: number) {
+    let items: Item[] = [];
     try {
-        const items = await client.v2.listItems();
-        const data = items.items
-        for (let i:number = 0; i < 6; i++) {
+        const itemList = await client.v2.listItems();
+        const data = itemList.items;
+        for (let i:number = 0; i < quantity; i++) {
             let index: number;
             do {
                 index = Math.floor(Math.random() * data.length--)
-            } while (data[index].type.id != 'outfit')
-            console.log(data[index].type + " " + data[index].name)
+            } while (data[index].type.id !== 'outfit' || data[index].price <= 0)
+            items.push(new Item(data[index].name, data[index].price, data[index].rarity.id, data[index].type.name))
         }
-
-        //new Item()
-        /*data.forEach((item: any) => {
-            console.log(item.type)
-        })*/
     } catch (error) {
         console.error("Une erreur s'est produite lors de la récupération des éléments:", error);
     }
+    return items;
 }
 
-getAndLogItems();
+getFeaturedItem(2).then(r => r.forEach(item => console.log(item.name, item.price, item.type, item.rarity)))
